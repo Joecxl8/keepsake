93c8ac5ba6a5893bf927a1dadef6b874d3d0cf59
Joe Manu Thomas <94967413+Joecxl8@users.noreply.github.com>
2026-09-02 19:31:42 +0530

    Add files via upload

---
+++ keepsake-app/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'keepsake-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
